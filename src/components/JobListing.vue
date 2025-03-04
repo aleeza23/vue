73f5a825a2e04a6e66d70cad094e19b154d907a8
Aleeza Rubab <malikaleeza886@gmail.com>
2025-03-04 04:46:04 -0700

    feat: created views

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps, ref, computed } from 'vue';
+import { RouterLink } from 'vue-router';
 
 const props = defineProps({
     job: Object,
@@ -30,8 +31,9 @@ const truncatedDescription = computed(() => {
 
             <div class="mb-5">
                 <div> {{ truncatedDescription }}</div>
-                <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5 cursor-pointer">{{ showFullDescription ? "Less" :
-                    "More"}}</button>
+                <button @click="toggleFullDescription"
+                    class="text-green-500 hover:text-green-600 mb-5 cursor-pointer">{{ showFullDescription ? "Less" :
+                        "More" }}</button>
             </div>
 
             <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -40,13 +42,14 @@ const truncatedDescription = computed(() => {
 
             <div class="flex flex-col lg:flex-row justify-between mb-4">
                 <div class="text-orange-700 mb-3">
-                    <i class="fa-solid fa-location-dot text-lg"></i>
+                    <i class="pi pi-map-marker text-orange-700"></i>
                     {{ job.location }}
                 </div>
-                <a :href="'/jobs/' + job.id"
+                <RouterLink :to="`/jobs/${job.id}`"
                     class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
-                </a>
+                </RouterLink>
+
             </div>
         </div>
     </div>
