dae8dddcce3d36476b01c95cb074764217754141
Aleeza Rubab <malikaleeza886@gmail.com>
2025-03-07 05:09:04 -0700

    created pages

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -15,7 +15,7 @@ const state = reactive({
 
 onMounted((async () => {
     try {
-        const res = await axios.get(`http://localhost:5000/jobs/${jobId}`);
+        const res = await axios.get(`https://vueserver-production.up.railway.app/jobs/${jobId}`);
         state.job = res.data
     } catch (error) {
         console.error("Error fetching job.", error)
